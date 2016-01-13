015213cc87d439940cd9b7525a0740ba9826be1c
Stephan <stephan@soundcloud.com>
2016-01-13 05:07:04 +0100

    Adding feature to add up configuration sets

--- README.md
+++ README.md
@@ -8,7 +8,17 @@ Filters BrowserStack flat map of browser configs from REST API and outputs a JSO
 
 Put `any` where you want all possibilies.
 
+**Trim spaces for all values that have spaces!**
+
+For example `OS X` -> `OSX`, `Mountain Lion` -> `MountainLion`, `Motorolla Razor` -> `MotorollaRazor` etc ...
+
 **Expects a file `browserstack.options.json` containing source data in working directory.**
+
+**Adding up configurations**
+
+You can add up several configuration sets. One set consisting of: `<os> <os_version> <browser> <browser_version> <device>`, further abreviated as `<set>`:
+
+`browserstack-select <set> + <set> + ...` will add up results of each set selected.
 
 Source data can be retrieved from BrowserStack API with a command like:
 
@@ -19,7 +29,7 @@ Source data can be retrieved from BrowserStack API with a command like:
 We want a config for Chrome 47.0 on all Windows versions.
 
 ```
-$ node browserstack-select.js Windows any chrome 47.0 any
+$ browserstack-select Windows any chrome 47.0 any
 [ { os_version: 'XP',
     browser_version: '47.0',
     device: null,
@@ -45,4 +55,20 @@ $ node browserstack-select.js Windows any chrome 47.0 any
     device: null,
     os: 'Windows',
     browser: 'chrome' } ]
+  ```
+
+  ## Example: adding up several sets
+
+  ```
+  $ browserstack-select Windows 8 chrome 47.0 any + OSX MountainLion chrome 47.0 any
+    [ { browser_version: '47.0',
+        os: 'OS X',
+        browser: 'chrome',
+        device: null,
+        os_version: 'Mountain Lion' },
+      { browser_version: '47.0',
+        os: 'Windows',
+        browser: 'chrome',
+        device: null,
+        os_version: '8' } ]
   ```
--- browserstack-select.js
+++ browserstack-select.js
@@ -1,41 +1,78 @@
 #!/usr/bin/env node
 
 var path = require('path');
-
 var args = Array.prototype.slice.call(process.argv, 2);
 
-//console.log(args);
+var CARDINALITY = 5;
 
-if (!args.length) {
+function printUsage() {
   console.log('\nbrowserstack-select: Filters BrowserStack flat map of browser configs from REST API and outputs a JSON array fit to be used with browserstack.json config for browserstack-runner.\n\nUsage: `node browserstack-select.js <os> <os_version> <browser> <browser_version> <device>`\n(put `any` where you want all possibilies)');
   console.log('\nExpects a file `browserstack.options.json` containing source data in working directory.');
   console.log('Source data can be retrieved from BrowserStack API with a command like `curl -u "USERNAME:PASSWORD" https://www.browserstack.com/automate/browsers.json > browserstack.options.json`');
   console.log('');
-  return;
+}
+
+if (!args.length) {
+  printUsage();
+  process.exit(1);
+}
+
+function validArgsLength() {
+  return (args.length + 1) % (CARDINALITY + 1) === 0;
+}
+
+function getArgsOffset() {
+  return (args.length + 1) / (CARDINALITY + 1);
+}
+
+if (!validArgsLength()) {
+  console.error('Invalid arguments length: %d\n\n', args.length);
+  printUsage();
+  process.exit(1);
 }
 
 var output = [];
-
 var options = require(path.resolve('./browserstack.options.json'));
+var offset = getArgsOffset();
 
-//console.log(options);
+while(offset-- > 0) {
 
-options.forEach(function(item) {
+  options.forEach(function(item) {
 
-  function checkArg(field, argIndex) {
-    return (item[field] === args[argIndex] || item[field] === null || 'any' === args[argIndex]);
-  }
+    function checkValidSeperator() {
+      var sep = getArg(5);
+      return sep === undefined || sep === '+';
+    }
 
-  if (
-       checkArg('device', 4)
-    && checkArg('browser_version', 3)
-    && checkArg('browser', 2)
-    && checkArg('os_version', 1)
-    && checkArg('os', 0)
-    ) {
-    output.push(item);
-  }
+    function getArg(index) {
+      return args[index + (CARDINALITY + 1) * offset];
+    }
 
-});
+    function getFieldValue(field) {
+      return item[field] ? item[field].replace(/ /g, '') : null;
+    }
+
+    function checkArg(field, argIndex) {
+      return (getFieldValue(field) === getArg(argIndex) || getFieldValue(field) === null || 'any' === getArg(argIndex));
+    }
+
+    if(!checkValidSeperator()) {
+      console.error('Invalid seperator: ' + getArg(5));
+      process.exit(1);
+    }
+
+    if (
+         checkArg('device', 4)
+      && checkArg('browser_version', 3)
+      && checkArg('browser', 2)
+      && checkArg('os_version', 1)
+      && checkArg('os', 0)
+      ) {
+      output.push(item);
+    }
+
+  });
+
+}
 
 console.log(output);
